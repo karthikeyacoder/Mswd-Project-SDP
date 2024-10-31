22f57dbfc55e702b2d4f4d3746fd7395a0c14a44
respostorydev <147632146+karthikeyacoder@users.noreply.github.com>
2024-10-31 21:05:58 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -6,7 +6,7 @@ import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 import karthikeya_admin from'./karthikeya_admin.png'
 
-export const url = 'http://localhost:4000'
+export const url = 'https://mswd-project-sdp-backend.onrender.com'
 export const currency = '₹'
 
 export const assets ={
